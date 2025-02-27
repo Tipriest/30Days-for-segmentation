4c7b4e270f9ca713a92af045babd19f3fb90a9f4
Tipriest <a1503741059@163.com>
2025-02-27 08:57:52 +0800

    llm_anno.py add new questions, readme.md add new ideas

--- steps/2_openword_annotation/llm_anno.py
+++ steps/2_openword_annotation/llm_anno.py
@@ -5,7 +5,9 @@ import base64
 from io import BytesIO
 import csv
 import json
-from datetime import datetime
+from runpy import run_path
+
+# from datetime import datetime
 
 from IPython.display import HTML, display
 from PIL import Image
@@ -26,7 +28,9 @@ def save_to_csv(_results, output_path="labels_llm_pred.csv"):
             #     ]
             # )
             # 写入CSV（例如：frame0001.jpg -> 草地,水面）
-            writer.writerow([_result["image_path"], ",".join(_result["annotations"])])
+            writer.writerow(
+                [_result["image_path"], ",".join(_result["annotations"])]
+            )
 
 
 def convert_to_base64(_pil_image):
@@ -88,13 +92,44 @@ def parse_args():
         #             Output the annotation result in csv format like this ["red brick road", "curb", ...]
         #             Caution don't output other things, just the annotation result, thanks
         #         """,
-        default="""You are a professional annotator for terrain classification data sets.
-                    Here is a list of annotations:
+        default="""Here is a list of terrain related annotations:
                     ["cement road", "red brick road", "yellow brick road", "soil",
                     "lawn", "water", "curb", "others",],
-                    Determine which annotations should be given in the picture?
-                    Use Json format like this {"annotations": ["soil"]} to return the result data.
+                    Determine which annotations of this list occured in this picture?
+                    Use Json format like this {"annotations": ["soil", "lawn", "yellow brick road", ...]} to return the result data.
+                    mention don't use other annotations beyond this list.
                 """,
+        # default="""
+        #             As a professional topography analysis assistant, please analyze images strictly according to the following rules:
+        #             1. Terrain category definition:
+        #             - Road category:
+        #             * cement road: grey continuous hardening road surface, which may contain obvious seams or cracks
+        #             * red brick road: paved road with red bricks arranged regularly
+        #             * yellow brick road: paved road with bright yellow bricks and possibly diamond-shaped arrangements
+        #             * curb: The raised strip structure at the edge of the road, which usually forms a height difference of 5-15cm from the road surface.
+        #             - Nature:
+        #             * soil: bare soil or sand, no vegetation coverage
+        #             * lawn: A grassy region
+        #             * water: liquid water surface (including accumulated water, rivers, etc.)
+        #             * others: Special terrain that is obviously not part of the above category
+
+        #             2. Analysis steps:
+        #              Focus on observing ground areas and ignore non-terrain elements such as buildings and sky
+        #              Identify the material characteristics (color, texture, structure) in each area
+        #              For questions:
+        #             - The color characteristics of the brick road must be clearly defined
+
+        #             3. Output requirements:
+        #             - Use strict JSON format
+        #             - Empty results return to empty list
+        #             - No comment description
+        #             - return annotations only in the list of ["cement road", "red brick road", "yellow brick road", "soil",
+        #             # "lawn", "water", "curb", "others"], some words in this list like red brick road shuould be considered as 
+        #             an entity and could not be seperated,  don't use other annotations, this is vital important.
+
+        #             Please return the JSON result that meets the above criteria and follow the following example to return
+        #             {"annotations": ["soil", "lawn", "yellow brick road", ...]}
+        #             """,
         # default="你是一个地形分类数据集的专业标注员, 判断一下图片中应该给那些标注? 以csv格式输出标注结果。",
         # default="""
         #         You are a professional annotator for terrain classification data sets.
@@ -124,9 +159,9 @@ if __name__ == "__main__":
 
     # 定义模型
     llm = ChatOllama(
-        model="llava",
+        model="llava:13b",
         # model="deepseek-r1:14b",
-        temperature=0.2,
+        temperature=0.5,
         format="json",
     )
 
@@ -165,10 +200,10 @@ if __name__ == "__main__":
             )
 
     if results:
-        output_filename = (
-            f"annotations_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
-        )
+        output_filename = "./steps/2_openword_annotation/labels.csv"
         save_to_csv(results, output_filename)
         print(f"Saved {len(results)} results to {output_filename}")
     else:
         print("No result to save")
+
+    run_path("./steps/2_openword_annotation/calF1.py")
